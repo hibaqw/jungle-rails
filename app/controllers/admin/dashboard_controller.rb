91dc0819840522c81a735225574e647d59901b7a
Hibaq <hibaqwarsam79@gmail.com>
2023-03-10 04:58:04 +0000

    added count for products and categories for admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products = Product.all.order(created_at: :desc)
+    @category = Category.all()
   end
 end
